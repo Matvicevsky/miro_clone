85e714d474596b6b5b0a3feb542f48b404a44b55
Ilya <matvicevsky_ilya@mail.ru>
2025-05-17 17:53:23 +0300

    Add api and mocks data

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,12 +1,68 @@
+import { rqClient } from '@/shared/api/instance'
 import { CONFIG } from '@/shared/model/config'
 import { ROUTES } from '@/shared/model/routes'
+import { useQueryClient } from '@tanstack/react-query'
 import { Link, href } from 'react-router-dom'
 
 function BoardsListPage() {
+	const queryClient = useQueryClient()
+	const boardsQuery = rqClient.useQuery('get', '/boards')
+
+	const createBoardMutation = rqClient.useMutation('post', '/boards', {
+		onSettled: async () => {
+			await queryClient.invalidateQueries(
+				rqClient.queryOptions('get', '/boards')
+			)
+		},
+	})
+	const deleteBoardMutation = rqClient.useMutation(
+		'delete',
+		'/boards/{boardId}',
+		{
+			onSettled: async () => {
+				await queryClient.invalidateQueries(
+					rqClient.queryOptions('get', '/boards')
+				)
+			},
+		}
+	)
+
 	return (
 		<div>
 			<h4>Boadrs List {CONFIG.API_BASE_URL}</h4>
-			<Link to={href(ROUTES.BOARD, { boardId: '1' })}>Board id: 1</Link>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault()
+					const formData = new FormData(e.target as HTMLFormElement)
+					createBoardMutation.mutate({
+						body: {
+							name: formData.get('name') as string,
+						},
+					})
+				}}
+			>
+				<input name='name' type='text' />
+				<button type='submit' disabled={createBoardMutation.isPending}>
+					Create board
+				</button>
+			</form>
+			{boardsQuery.data?.map((board) => (
+				<div key={board.id}>
+					<Link to={href(ROUTES.BOARD, { boardId: board.id })}>
+						{board.name}
+					</Link>
+					<button
+						disabled={deleteBoardMutation.isPending}
+						onClick={() =>
+							deleteBoardMutation.mutate({
+								params: { path: { boardId: board.id } },
+							})
+						}
+					>
+						Delete
+					</button>
+				</div>
+			))}
 		</div>
 	)
 }
